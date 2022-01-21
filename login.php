a1ef04dc26c465a692888bfc6db66f120e01b113
fahritech04 <fahricoding@gmail.com>
2022-01-21 14:12:42 +0800

    Delete Login

--- login.php
+++
@@ -1,8 +0,0 @@
-<img src="" alt="Logo App Data Siswa">
-<h1>DATA SISWA</h1>
-<H3>Sistem Informasi Data Siswa</H3>
-<form action="cek_login.php" method="POST">
-    <input type="text" name="username" class="form-input">
-    <input type="password" name="password" class="form-input">
-    <input type="submit" name="login" value="login">
-</form>
